016df4f6e4e75a404a5172a6a5c616afa1f7b4f0
matheus-rossi <matheus_rossi@outlook.com>
2017-04-16 18:43:23 -0300

    chat basic routes

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on("connection", (socket) => {
+    console.log("New user Connected");
+})
+
+server.listen(port, () => {
+    console.log(`Chat Running! on ${port}`)
+})
