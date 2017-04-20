331170a4f6de231c117096332f66e2a0b9a3af4a
matheus-rossi <matheus_rossi@outlook.com>
2017-04-20 20:51:23 -0300

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -16,12 +16,43 @@ socket.on("newMessage", function(message) {
     $("#messages").append(li);
 });
 
+socket.on("newLocationMessage", function(message) {
+    var li = $("<li></li>");
+    var a = $(`<a target="_blank">My current location</a>`);
+    li.text(`${message.from}: `);
+    a.attr("href", message.url);
+    li.append(a);
+    $("#messages").append(li);
+})
+
+
 $("#message-form").on("submit", function(e) {
     e.preventDefault();
+    var messageTextBox = $("[name=message]");
     socket.emit("createMessage", {
         from: "User",
-        text: $("[name=message]").val()
+        text: messageTextBox.val()
     }, function() {
-
+        messageTextBox.val("");
     });
+});
+
+var locationButton = $("#send-location");
+
+locationButton.on("click", function() {
+    if (!navigator.geolocation) {
+        return alert("Geolocation not suported by your browser");
+    }
+    locationButton.attr("disabled", "disabled").text("Sending Location ...");
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr("disabled").text("Send Location");
+        socket.emit("createLocationMessage", {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        console.log("Unable to fetch location");
+        locationButton.removeAttr("disabled").text("Send Location");
+    })
+
 });
