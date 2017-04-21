b202755a426536be6ee2939398aa97a87383896c
matheus-rossi <matheus_rossi@outlook.com>
2017-04-21 11:17:11 -0300

    mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,25 @@ socket.on("disconnect", function() {
 
 socket.on("newMessage", function(message) {
     let formatedTime = moment(message.createdAt).format("h:mm a");
-    console.log("newMessage", message)
-    let li = $("<li></li>");
-    li.text(`${message.from} ${formatedTime}: ${message.text}`);
-
-    $("#messages").append(li);
+    let template = $("#message-template").html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    });
+    $("#messages").append(html);
 });
 
 socket.on("newLocationMessage", function(message) {
     let formatedTime = moment(message.createdAt).format("h:mm a");
-    var li = $("<li></li>");
-    var a = $(`<a target="_blank">My current location</a>`);
-    li.text(`${message.from} ${formatedTime}: `);
-    a.attr("href", message.url);
-    li.append(a);
-    $("#messages").append(li);
+    let template = $("#location-message-template").html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime,
+        url: message.url
+    });
+    $("#messages").append(html);
 })
 
 
